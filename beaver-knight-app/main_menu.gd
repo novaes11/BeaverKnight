867eb3ab170e2b35158b8f57982d0ea54e3463ee
jcamargo26 <j.camargo@edu.unifil.br>
2026-08-23 17:13:22 -0300

    feat: adiciona tela de ajuda e finaliza botoes do menu

--- beaver-knight-app/main_menu.gd
+++ beaver-knight-app/main_menu.gd
@@ -13,3 +13,21 @@ func _process(delta: float) -> void:
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://town.tscn") # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	$MenuPanel.hide()
+	$LeafFrame.hide()
+	$HelpPanel.show()
+	$HelpPanel/HelpContent/BackButton.grab_focus()
+
+
+func _on_back_button_pressed() -> void:
+	$HelpPanel.hide()
+	$MenuPanel.show()
+	$LeafFrame.show()
+	$MenuPanel/Buttons/StartButton.grab_focus()
+	
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
