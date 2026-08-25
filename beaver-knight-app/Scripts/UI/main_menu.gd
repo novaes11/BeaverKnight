850204795e2d8bca6e8588dad231e120a813c8af
EnzoSchmidt-1 <enzo.schmidt@edu.unifil.br>
2026-08-25 19:53:12 -0300

    feat: Aplicando corretamente as musicas e organizando em pastas
    Consertei o erro causado pela commit anterior reaplicando os inimigos no, jogo tambem adicionando musica ao jogo, é organizando todos os arquivos em pastas designadas

--- beaver-knight-app/Scripts/UI/main_menu.gd
+++ beaver-knight-app/Scripts/UI/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://town.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Scene/town.tscn") # Replace with function body.
 
 
 func _on_options_button_pressed() -> void:
