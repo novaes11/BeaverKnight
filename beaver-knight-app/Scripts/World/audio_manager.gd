788497d8f7c83b8ea0bbbd91d630ac652b5690d6
EnzoSchmidt-1 <enzo.schmidt@edu.unifil.br>
2026-09-08 20:27:38 -0300

    feat: Atualizando o AudioManager
    Criando funcionalidades do áudio manager para possibilitar utiliza-lo para controlar áudio posicional e musica

--- beaver-knight-app/Scripts/World/audio_manager.gd
+++ beaver-knight-app/Scripts/World/audio_manager.gd
@@ -1,37 +1,78 @@
 extends Node
 
-var musica_volume: int
-
-var sfx_volume: int
 
 #2 Variaveis que definem a quantidade de audios que podem tocar ao mesmo tempo
 var pool_Tamanho: int = 8
-var tocador: Array[AudioStreamPlayer] = []
 
-#Cria o Array da pool Imediatamente
+# Pool de áudio Global
+var sfx_players: Array[AudioStreamPlayer] = []
+
+# Pool de áudio Posicional
+var sfx_2d_players: Array[AudioStreamPlayer2D] = []
+
+# Player de musica
+var musica_player: AudioStreamPlayer
+
+#Cria os Arrays de AudioStreamPlayer
 func _ready() -> void:
+	# pool de efeitos sonoros
+	for i in range(pool_Tamanho):
+		var pool = AudioStreamPlayer.new()
+		pool.bus = "Effect"
+		add_child(pool)
+		sfx_players.append(pool)
+	
+	# pool de efeitos posicionais
 	for i in range(pool_Tamanho):
 		var pool = AudioStreamPlayer.new()
 		add_child(pool)
-		tocador.append(pool)
+		pool.bus = "Effect"
+		add_child(pool)
+		sfx_2d_players.append(pool)
+	
+	# Define a musica tocada atualmente
+	musica_player = AudioStreamPlayer.new()
+	musica_player.bus = "Music"
+	add_child(musica_player)
+	
 
-#Função que toca os sons
+
+# Funções que controlam os efeitos sonoros
 func play_sfx(stream: AudioStream, volume_db: float = 0.0) -> void:
 	#cancela se não foi achado audio
 	if stream == null: 
 		return
 	
 	#Procura um tocador livre
-	for pool in tocador:
+	for pool in sfx_players:
 		if ! pool.playing:
 			pool.stream = stream
 			pool.volume_db = volume_db
 			pool.play()
+
+func play_sfx_2d(stream: AudioStream, volume_db: float = 0.0) -> void:
+	if stream == null:
+		return
+	
+	for pool_2d in sfx_2d_players:
+		if ! pool_2d.playing:
+			pool_2d.stream = stream
+			pool_2d.volume = volume_db
+			pool_2d.play()
 			return
-		
+
+# Função que controla a musica do jogo
+func play_music(stream: AudioStream, volume_db: float = 0.0) -> void:
+	if stream == null:
+		return
+	# Se a mesma música já estiver tocando, não reinicia
+	if musica_player.stream == stream and musica_player.playing:
+		return
 	
-	#Se estiver tudo cheio substitui o primeiro
-	tocador[0].stream = stream
-	tocador[0].volume_db = volume_db
-	tocador[0].play()
-	
+	musica_player.stream = stream
+	musica_player.volume_db = volume_db
+	musica_player.play()
+
+# Para a musica que esta sendo tocada
+func stop_music():
+	musica_player.stop()
