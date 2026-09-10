aa5895a05534a96cb1f0c09e5c61c19486f551ed
EnzoSchmidt-1 <enzo.schmidt@edu.unifil.br>
2026-09-10 01:19:25 -0300

    feat: Implementado as musicas
    Musica da cidade:
    https://pixabay.com/music/video-games-against-villainous-robot-148967/
    
    Criador: Não encontrei
    
    Musica do menu:
    https://pixabay.com/pt/music/jogos-de-v%C3%ADdeo-8bit-video-game-music-289970/
    
    Criador: ArthurHale

--- beaver-knight-app/Scripts/UI/main_menu.gd
+++ beaver-knight-app/Scripts/UI/main_menu.gd
@@ -1,11 +1,15 @@
 extends Control
 
 @export var sfx_botao: AudioStream
+@export var musica_menu: AudioStream
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	AudioManager.stop_music()
+	AudioManager.play_music(musica_menu, -20.0)
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,8 +17,9 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scene/town.tscn") # Replace with function body.
 	AudioManager.play_sfx(sfx_botao, -28.0)
+	get_tree().change_scene_to_file("res://Scene/town.tscn") # Replace with function body.
+	
 
 func _on_options_button_pressed() -> void:
 	$MenuPanel.hide()
